78065667e4598518a55df962c84db9e4fcdb952b
FelipeMoreira <130478384+FelipeMMares@users.noreply.github.com>
2024-09-11 19:54:53 -0300

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule],// Importando módulos necessários diretamente aqui
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
 
+  constructor(private fb: FormBuilder, private apiService: ApiService) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.apiService.login(username, password).subscribe({
+        next: (response) => {
+          // Lógica de sucesso, como redirecionar ou armazenar o token
+          console.log('Login bem-sucedido', response);
+        },
+        error: (error) => {
+          // Exibir mensagem de erro
+          this.errorMessage = 'Credenciais inválidas';
+        }
+      });
+    }
+  }
 }
